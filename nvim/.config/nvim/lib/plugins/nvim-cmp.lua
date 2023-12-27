57d49f1de957c6190409609d7a52b5684cb30be4
Adam Kessler <adamvonbaron@icloud.com>
2023-12-26 22:48:38 -0500

    oops

--- nvim/.config/nvim/lib/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lib/plugins/nvim-cmp.lua
@@ -11,7 +11,7 @@ return {
                     luasnip.lsp_expand(args.body)
                 end
             },
-            mapping = {
+            mapping = cmp.mapping.preset.insert({
                 ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
                 ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
                 ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
@@ -21,7 +21,7 @@ return {
                     c = cmp.mapping.close(),
                 }),
                 ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            },
+            }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' },

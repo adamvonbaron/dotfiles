bd2696608e8f70f5afa694a0c3f424d93b14ef11
Adam Kessler <adamvonbaron@icloud.com>
2023-12-27 19:31:12 -0500

    reenable icons

--- nvim/.config/nvim/lib/plugins/lualine-nvim.lua
+++ nvim/.config/nvim/lib/plugins/lualine-nvim.lua
@@ -1,15 +1,14 @@
 return {
-    "hoob3rt/lualine.nvim",
-    config = function()
-        local lualine = require('lualine')
+  "hoob3rt/lualine.nvim",
+  config = function()
+    local lualine = require('lualine')
 
-        lualine.setup({
-          options = {
-            theme = "auto",
-            icons_enabled = false,
-            component_separators = '|',
-            section_separators = '',
-          }
-        })
-    end
+    lualine.setup({
+      options = {
+        theme = "auto",
+        component_separators = '|',
+        section_separators = '',
+      }
+    })
+  end
 }

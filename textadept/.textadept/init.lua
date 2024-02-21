d5a096105ede1222a0cb6e3b104efbb91930549d
Adam Kessler <adamvonbaron@icloud.com>
2024-02-20 19:20:52 -0500

    textadept

---
+++ textadept/.textadept/init.lua
@@ -0,0 +1,28 @@
+local lsp = require('lsp')
+
+
+if not CURSES then
+	view:set_theme('light', {font = 'Lucida Console', size=10})
+end
+
+buffer.use_tabs = false
+buffer.tab_width = 4
+
+-- autoreload buffers from disk (usually due to formatters being run)
+events.connect(events.FILE_CHANGED, function(filename)
+   buffer:reload()
+   return true
+end, 1)
+
+keys['ctrl+f12'] = lsp.goto_declaration
+
+lsp.log_rpc = true
+lsp.server_commands.cpp = 'clangd'
+lsp.server_commands.ansi_c = 'clangd'
+lsp.server_commands.go = 'gopls'
+lsp.server_commands.lua = 'lua-language-server'
+
+textadept.editing.highlight_words = textadept.editing.HIGHLIGHT_SELECTED
+textadept.editing.auto_enclose = true
+
+ui.find.highlight_all_matches = true

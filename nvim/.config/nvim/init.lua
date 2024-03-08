27d984057dc0f91999b7fbcb85cc5f5f0cfcfc46
Adam Kessler <adamvonbaron@icloud.com>
2024-03-08 14:09:36 -0500

    make package path os dependent

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,10 @@
-package.path = package.path .. ";/Users/adam/.config/nvim/lib/?.lua"
+local linux = os.getenv("OSTYPE") == "linux"
+
+if linux then
+	package.path = package.path .. ";/home/adam/.config/nvim/lib/?.lua"
+else
+	package.path = package.path .. ";/Users/adam/.config/nvim/lib/?.lua"
+end
 
 require("plugins")
 require("basic")

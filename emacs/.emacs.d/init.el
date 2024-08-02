d15e584486ba8da09c6db268c85d263f76da0971
Adam Kessler <exitsnil@gmail.com>
2024-08-02 19:45:23 -0400

    add notmuch

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -82,6 +82,9 @@
 ;; typescript
 (add-hook 'ts-mode-hook 'eglot-ensure) ; typescript-language-server
 
+;; notmuch
+(require 'notmuch)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

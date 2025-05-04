29c9b2030d3d96110ce9ac8211ccefd2e459e9b8
Adam Kessler <exitsnil@gmail.com>
2025-05-04 14:41:56 -0400

    ok

--- nvim/.config/nvim/lib/plugins.lua
+++ nvim/.config/nvim/lib/plugins.lua
@@ -39,10 +39,10 @@ lazy.setup({
     require('plugins.trouble-nvim'),
     require("plugins.lspsaga-nvim"),
     require("plugins.symbols-outline-nvim"),
-    require("plugins.tokyo-night-nvim"),
     require("plugins.nvim-web-devicons"),
     require("plugins.nvim-lualine"),
     require("plugins.nvim-treesitter"),
-    require("plugins.copilot-vim")
+    require("plugins.copilot-vim"),
+    require("plugins.vscode-nvim"),
   }
 })

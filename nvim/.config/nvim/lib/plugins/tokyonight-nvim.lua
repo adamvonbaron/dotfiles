5f1e5b4542f073744ca070e25c56b0c8b0543c66
Adam Kessler <exitsnil@gmail.com>
2025-07-17 20:30:55 -0400

    stylua

--- nvim/.config/nvim/lib/plugins/tokyonight-nvim.lua
+++ nvim/.config/nvim/lib/plugins/tokyonight-nvim.lua
@@ -1,15 +1,15 @@
 return {
 	"folke/tokyonight.nvim",
-  config = function()
-    local tokyonight = require("tokyonight")
+	config = function()
+		local tokyonight = require("tokyonight")
 
-    tokyonight.setup({
-      on_colors = function(colors)
-        colors.bg = "#000000"
-        colors.bg_dark = "#000000"
-        colors.bg_sidebar = "#000000"
-        colors.bg_statusline = "#000000"
-    end
-    })
-  end
+		tokyonight.setup({
+			on_colors = function(colors)
+				colors.bg = "#000000"
+				colors.bg_dark = "#000000"
+				colors.bg_sidebar = "#000000"
+				colors.bg_statusline = "#000000"
+			end,
+		})
+	end,
 }

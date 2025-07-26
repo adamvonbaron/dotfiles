1df5a2342ee9874a41fde9241040d0125dbed9ff
Adam Kessler <exitsnil@gmail.com>
2025-07-26 12:09:33 -0400

    fix trouble

--- nvim/.config/nvim/lib/plugins/trouble-nvim.lua
+++ nvim/.config/nvim/lib/plugins/trouble-nvim.lua
@@ -4,38 +4,38 @@ return {
 		local trouble = require("trouble")
 		trouble.setup({
 			use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
-			icons = false,
-			indent_lines = false,
-			fold_open = "v",
-			fold_close = ">",
-			signs = {
-				error = "error",
-				warning = "warn",
-				hint = "hint",
-				information = "info",
-			},
 		})
-
-		vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { silent = true, noremap = true })
-		vim.keymap.set(
-			"n",
-			"<leader>xw",
-			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-			{ silent = true, noremap = true }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>xd",
-			"<cmd>Trouble symbols toggle focus=false<cr>",
-			{ silent = true, noremap = true }
-		)
-		vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>", { silent = true, noremap = true })
-		vim.keymap.set("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>", { silent = true, noremap = true })
-		vim.keymap.set(
-			"n",
-			"gR",
-			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-			{ silent = true, noremap = true }
-		)
 	end,
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
 }

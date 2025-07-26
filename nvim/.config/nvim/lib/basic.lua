17fde83ec946f00d637f0d0b667ef1acc29a249b
Adam Kessler <exitsnil@gmail.com>
2025-07-26 11:59:35 -0400

    lackluster

--- nvim/.config/nvim/lib/basic.lua
+++ nvim/.config/nvim/lib/basic.lua
@@ -14,7 +14,7 @@ vim.opt.completeopt = "menu,menuone,noselect"
 vim.opt.signcolumn = "no"
 vim.opt.nu = true
 -- vim.cmd("colorscheme quiet")
-vim.cmd("colorscheme tokyonight-night")
+vim.cmd("colorscheme lackluster")
 vim.opt.termguicolors = true
 -- vim.opt.background = "light"
 
--- nvim/.config/nvim/lib/plugins/nvim-lualine.lua
+++ nvim/.config/nvim/lib/plugins/nvim-lualine.lua
@@ -7,8 +7,7 @@ return {
 		lualine.setup({
 			options = {
 				icons_enabled = true,
-				-- theme = "auto",
-				theme = "tokyonight",
+				theme = "lackluster",
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = {

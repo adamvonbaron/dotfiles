0a4f3306db77c7f0b5c4d223fae17dcd7be51a6a
Adam Kessler <exitsnil@gmail.com>
2025-07-28 21:12:12 -0400

    add claude code keybinds

--- nvim/.config/nvim/lib/plugins/claude-code.lua
+++ nvim/.config/nvim/lib/plugins/claude-code.lua
@@ -5,7 +5,10 @@ return {
 	},
 	config = function()
 		local claude_code = require("claude-code")
-
 		claude_code.setup()
+
+		vim.keymap.set("n", "<leader>cc", "<cmd>ClaudeCode<CR>")
+		vim.keymap.set("n", "<leader>ccr", "<cmd>ClaudeCodeResume<CR>")
+		vim.keymap.set("n", "<leader>ccc", "<cmd>ClaudeCodeContinue<CR>")
 	end,
 }
